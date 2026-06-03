8753bf4fe6092bf79d89909062dc7b049b419612
Rafik <rafikrezig7@gmail.com>
2026-06-03 09:50:48 +0100

    1st video

---
+++ 10-Beginner - Functions with Outputs/main.py
@@ -0,0 +1,13 @@
+#.title turns a word into title by making the 1st word letter in caps and the otheres in lowercase
+# def format_name(f_name, l_name):
+#     formated_f_name=f_name.title()
+#     formated_l_name=l_name.title()
+#     return f"{formated_f_name}{formated_l_name}"
+# print(format_name("AngELa","yU"))
+
+def fucntion_1(text):
+    return text+text
+def function_2(text):
+    return text.title()
+output = function_2(fucntion_1("hello"))
+print(output)
